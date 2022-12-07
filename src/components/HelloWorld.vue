a2d4b1b9a8ceb5c6671083d1cf584de7a31a0437
김기현 <kimkee7@gmail.com>
2022-12-07 14:46:03 +0900

    firebase시작1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h1>{{ db }}dsf</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -34,7 +35,17 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
+    db: String,
+  },
+  created(){
+    console.log("created");
+    this.read();
+  },
+  methods:{
+    async read(){
+      console.log();
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,28 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { initializeApp } from "firebase/app";
+import { getFirestore} from 'firebase/firestore';
 
-createApp(App).mount('#app')
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCnYJTa63-PRm899pb_va79ndU68Jkt_r8",
+    authDomain: "myworks-f1205.firebaseapp.com",
+    projectId: "myworks-f1205",
+    storageBucket: "myworks-f1205.appspot.com",
+    messagingSenderId: "248734016315",
+    appId: "1:248734016315:web:2259b601c7bd4ea68bcd1e",
+    measurementId: "G-KE1P2B2HB8"
+};
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+console.log(db);
+export default db;
+
+// const querySnapshot = await getDocs(collection(db, "test"));
+// console.log(querySnapshot);
+
+
+
+createApp(App).mount('#app');
