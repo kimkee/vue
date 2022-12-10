95d78fb0f82378a5f4696330a45274b0e30b9b3a
KimKeeHyun <kimkee7@gmail.com>
2022-12-10 22:36:49 +0900

    보기 어설프게 구성4

--- src/views/View.vue
+++ src/views/View.vue
@@ -4,8 +4,6 @@
       <h1>{{ msg }}</h1> 
       <!-- {{$route.params.id}} -->
 
-      <!-- {{Views}} -->
-
       <div class="board-view">
         <div class="dt">
           <h4 class="tits">{{ Views.title }} {{ Views.key }}</h4>
@@ -16,8 +14,8 @@
         </dd>
         <div class="btn-set">
           <router-link class="btn sm" to="/list">목록</router-link>
-          <!-- <router-link class="btn sm" to="/list">수정</router-link>
-          <router-link class="btn sm" to="/list">삭제</router-link> -->
+          <router-link class="btn sm" to="/list">수정</router-link>
+          <router-link class="btn sm" to="/list">삭제</router-link>
         </div>
       </div>
 
@@ -27,9 +25,8 @@
 
 <script>
 import db  from '../firebaseConfig.js';
-import { collection, query, getDocs, orderBy } from "firebase/firestore";
+import { getDoc, doc } from "firebase/firestore";
 import { useRoute } from 'vue-router';
-
 
 export default {
   name: 'ViewItem',
@@ -42,33 +39,31 @@ export default {
       }
   },
   created(){
-    console.log("list created");
-    console.log(db);
-    
-    
-
+    console.log("view created");
+    console.clear();
     const route = useRoute();  
-    const id = route.params.id; // read parameter id (it is reactive) 
-    this.view(id);
-    console.log(`페이지 :  ${id}`);
-
+    const ids = route.params.id; // read parameter id (it is reactive) 
+    this.view(ids);
   },
   mounted(){
     document.querySelector(".header .cdt .tit").textContent = '보기';
   },
   methods:{
     async view(ids){
-      const q = query(collection(db, "test"), orderBy("timestamp", "desc"));
-      const querySnapshot = await getDocs(q);
-      this.Boards = [];
-      querySnapshot.forEach( doc => {
-        console.log(doc.id);
-        if( doc.id == ids ){
-          this.Views.title = doc.data().title;
-          this.Views.content = doc.data().content;
-          this.Views.timestamp = new Intl.DateTimeFormat('ko-KR',{ dateStyle: 'full', timeStyle: 'medium'}).format( doc.data().timestamp.toDate() ) ;
-        }
-      });
+      const docRef = doc(db, "test" , ids);
+      try {
+        const docSnap = await getDoc(docRef);
+        console.log(`
+          제목 : ${docSnap.data().title} 
+          내용 : ${docSnap.data().content}
+        `);
+
+        this.Views.title = docSnap.data().title;
+        this.Views.content = docSnap.data().content;
+        this.Views.timestamp = new Intl.DateTimeFormat('ko-KR',{ dateStyle: 'full', timeStyle: 'medium'}).format( docSnap.data().timestamp.toDate() ) ;
+      } catch(error) {
+        console.log(error)
+      }
     }
 
   }
