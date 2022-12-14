923064c97ecf212ab6fd1ffd60d4f530d38410fa
KimKeeHyun <kimkee7@gmail.com>
2022-12-14 16:40:33 +0900

    댓글 UI 작업 4 입력시간

--- src/components/Comments.vue
+++ src/components/Comments.vue
@@ -10,7 +10,7 @@
               <div class="user"><a href="javascript:;" class="pic"><img src="" alt="사진" onerror="this.src = './img/user.png';" class="img"></a></div>
               <div class="infs">
                 <div class="name"><em class="nm">{{cmt.author}}</em></div>
-                <div class="desc"><em class="time">4일전</em></div>
+                <div class="desc"><em class="time">{{cmt.date}}</em></div>
                 <div class="ment" v-html="cmt.reply"></div>
                 <!-- <div class="rbt"><button type="button" class="bt repy">답글달기</button></div> -->
                 <!-- <div class="bts"><button type="button" class="bt accu">신고하기</button></div> -->
@@ -41,7 +41,7 @@
 
 <script>
 import db  from '../firebaseConfig.js';
-import { getDoc, doc  } from "firebase/firestore";
+import { getDoc, doc , updateDoc } from "firebase/firestore";
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { useRoute } from 'vue-router';
 
@@ -84,29 +84,29 @@ export default {
       });
     },
     async comtList(ids){
-      console.log("uid === " + ids);
       const docRef = doc(db, "bbs" , ids);
       try {
+        console.log("uid === " + ids);
           const docSnap = await getDoc(docRef);
           this.Coments = docSnap.data().coments || [] ;
           this.Coments.forEach((c, i) => {         
-            this.Coments[i] = ( JSON.parse(c) );
+            this.Coments[i] = ( c );
             this.Coments[i].reply = this.Coments[i].reply.replace(/\n/g,'<br>');
+            this.Coments[i].date = new Intl.DateTimeFormat('ko-KR',{ dateStyle: 'full', timeStyle: 'short'}).format( this.Coments[i].date );
+            
           });
           console.log(  `댓글 수 = ${ this.Coments.length }` );
       }catch(error) {
         console.log(error);
       }
+      console.log(this.Coments);
     },
-    comtSed(opt){
-      console.log("댓글 전송 =======");
-      console.table(opt);
-    },
+    
     comtWrite(e){
       const sendBtn = e.currentTarget;
       const inputReply =  sendBtn.closest(".ut-rpwrite").querySelector("#input_reply");
       if(inputReply.value == "") return;     
-
+/* 
       const rHTML = `
         <li>
           <div class="rpset">
@@ -122,18 +122,36 @@ export default {
         </li>`;
 
       document.querySelector(".ut-reply ul.rlist.a").insertAdjacentHTML("beforeend",rHTML);
-
+ */
+      const today = new Intl.DateTimeFormat('ko-KR',{ dateStyle: 'full', timeStyle: 'short'}).format( new Date() ) ;
+      // const today = new Date();
       this.comtSed({
+        author : "홍길동",
         uid : this.userInfo.uid,
         email : this.userInfo.email,
-        ment: inputReply.value.replace(/\n/g,'<br>'),
-        date: new Date()
+        reply: inputReply.value.replace(/\n/g,'<br>'),
+        date: today
       });
 
       // inputReply.style = ""
       inputReply.value = "";
       inputReply.focus();
       inputReply.dispatchEvent(new Event('input'));
+    },
+    async comtSed(opt){
+
+      console.log("댓글 전송 =======");
+      console.table(JSON.stringify(opt));
+
+      this.Coments.push(opt)
+      const thisDoc = doc(db, "bbs", this.userId);
+      await updateDoc ( thisDoc, {
+        coments : this.Coments
+      });
+
+
+
+
     },
     autoHeight(){ // 댓글에 자동높이 기능
       document.querySelector('[data-ui="autoheight"]').addEventListener("input",e =>{
