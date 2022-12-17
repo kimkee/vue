18c7ab5abd13e7830f50f617e1774d2d4040d9b2
KimKeeHyun <kimkee7@gmail.com>
2022-12-17 23:26:51 +0900

    bbs업그레이드 준비

--- src/views/Photo.vue
+++ src/views/Photo.vue
@@ -5,36 +5,36 @@
       <div class="ui-loading-dot"><div class="bx"><em><i></i></em></div></div>
       <div class="ut-tblist">
         <ul class="list" id="dp_list">
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/1.jpg" alt=""></div></a></div></li>
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/2.jpg" alt=""></div></a></div></li>
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/3.jpg" alt=""></div></a></div></li>
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/4.jpg" alt=""></div></a></div></li>
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/5.jpg" alt=""></div></a></div></li>
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/7.jpg" alt=""></div></a></div></li>
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/6.jpg" alt=""></div></a></div></li>
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/8.jpg" alt=""></div></a></div></li>
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/9.jpg" alt=""></div></a></div></li>
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/10.jpg" alt=""></div></a></div></li>
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/1.jpg" alt=""></div></a></div></li>
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/2.jpg" alt=""></div></a></div></li>
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/3.jpg" alt=""></div></a></div></li>
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/4.jpg" alt=""></div></a></div></li>
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/5.jpg" alt=""></div></a></div></li>
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/7.jpg" alt=""></div></a></div></li>
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/6.jpg" alt=""></div></a></div></li>
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/8.jpg" alt=""></div></a></div></li>
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/9.jpg" alt=""></div></a></div></li>
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/10.jpg" alt=""></div></a></div></li>
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/1.jpg" alt=""></div></a></div></li>
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/2.jpg" alt=""></div></a></div></li>
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/3.jpg" alt=""></div></a></div></li>
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/4.jpg" alt=""></div></a></div></li>
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/5.jpg" alt=""></div></a></div></li>
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/7.jpg" alt=""></div></a></div></li>
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/6.jpg" alt=""></div></a></div></li>
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/8.jpg" alt=""></div></a></div></li>
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/9.jpg" alt=""></div></a></div></li>
-          <li><div class="box"><a href="javascript:;"><div class="img"><img src="//kimkee.github.io/ui/static/img/temp/10.jpg" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/640/360/?v=1" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/360/640/?v=2" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/640/360/?v=3" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/360/640/?v=4" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/640/360/?v=5" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/360/640/?v=6" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/640/360/?v=7" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/360/640/?v=8" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/640/360/?v=9" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/360/640/?v=10" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/640/360/?v=11" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/360/640/?v=12" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/640/360/?v=13" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/640/360/?v=14" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/640/360/?v=15" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/640/360/?v=16" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/640/360/?v=17" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/640/360/?v=18" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/640/360/?v=19" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/640/360/?v=20" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/640/360/?v=21" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/640/360/?v=22" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/640/360/?v=23" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/640/360/?v=24" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/640/360/?v=25" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/640/360/?v=26" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/640/360/?v=27" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/640/360/?v=28" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/640/360/?v=29" alt=""></div></a></div></li>
+          <li><div class="box"><a href="javascript:;"><div class="pic"><img class="img" loading="lazy" src="//picsum.photos/640/360/?v=30" alt=""></div></a></div></li>
         </ul>
         <div class="ui-loadmore active">
           <em></em>
