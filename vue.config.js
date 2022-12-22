8a3adecfaf94959ce69533774af2795fa226b1b4
KimKeeHyun <kimkee7@gmail.com>
2022-12-22 15:00:39 +0900

    배포

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service');
 const path = require("path");
+const tit = "쀼:VUE";
+const d = new Date();
+const ver = (d.getMonth()+1) +""+ d.getDate() +""+ d.getHours() +""+ d.getMinutes() +""+ d.getSeconds();
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave:false,
@@ -7,5 +10,15 @@ module.exports = defineConfig({
   publicPath:"",
   configureWebpack: {
     watch: false
+  },
+  pages: {
+    index: { // (1)
+      entry: 'src/main.js', // (2)
+      template: 'public/index.html', // (3)
+      // filename: 'index.html', // (4)
+      title: tit, // (5)
+      version: ver, // (5)
+      // chunks: ['chunk-vendors', 'chunk-common', 'index'] // (6)
+    },
   }
 });
