61503a02f35ddcbced7f6356c656a4a52c772a27
KimKeeHyun <kimkee7@gmail.com>
2023-02-01 22:50:12 +0900

    --safe-top

--- src/router.js
+++ src/router.js
@@ -35,7 +35,7 @@ const router = createRouter({
             components: {
                 default: Chat,
                 HeaderSub,
-                Nav
+                
             }
         },
         {
