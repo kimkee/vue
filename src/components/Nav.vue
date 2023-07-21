b4e72875b4fc85b644593c40ea68a803ee1bddaa
KimKeeHyun <kimkee7@gmail.com>
2023-07-21 22:01:46 +0900

    class="isActive('home')

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,10 +2,10 @@
   <nav id="menubar" class="menubar">
     <div class="inr">
       <ul class="menu">
-        <li><router-link class="bt" to="/home"><i class="fa-regular fa-house"></i><em>Home</em></router-link></li>
-        <li><router-link class="bt" to="/bbs"><i class="fa-regular fa-list"></i><em>Board</em></router-link></li>
-        <li><router-link class="bt" to="/photo"><i class="fa-regular fa-camera"></i><em>Photo</em></router-link></li>
-        <li><router-link class="bt" to="/chat"><i class="fa-regular fa-comments"></i><em>Chat</em></router-link></li>
+        <li :class="isActive('home')"><router-link class="bt" to="/home"><i class="fa-regular fa-house"></i><em>Home</em></router-link></li>
+        <li :class="isActive('bbs')"><router-link class="bt" to="/bbs"><i class="fa-regular fa-list"></i><em>Board</em></router-link></li>
+        <li :class="isActive('photo')"><router-link class="bt" to="/photo"><i class="fa-regular fa-camera"></i><em>Photo</em></router-link></li>
+        <li :class="isActive('chat')"><router-link class="bt" to="/chat"><i class="fa-regular fa-comments"></i><em>Chat</em></router-link></li>
         <!-- <li><router-link class="bt" to="/works"><i class="fa-regular fa-briefcase"></i><em>Works</em></router-link></li> -->
         <li>
           <router-link v-if="!!$store.state.userInfo.uid" class="bt" to="/signout"><i class="fa-regular fa-right-from-bracket"></i><em>Logout</em></router-link>
@@ -25,7 +25,7 @@ export default {
   },
   data() {
       return {
-          
+        isActive : els => location.hash.includes(els) ? "active" : null
       }
   },
   created(){
