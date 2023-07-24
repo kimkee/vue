9b6bbe2aba7cf4927a3835fe27b08a6dce9fde56
KimKeeHyun <kimkee7@gmail.com>
2023-07-24 17:26:36 +0900

    class="isActive('home')

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -6,7 +6,7 @@
         <li :class="isActive('bbs')"><router-link class="bt" to="/bbs"><i class="fa-regular fa-list"></i><em>Board</em></router-link></li>
         <li :class="isActive('photo')"><router-link class="bt" to="/photo"><i class="fa-regular fa-camera"></i><em>Photo</em></router-link></li>
         <li :class="isActive('chat')"><router-link class="bt" to="/chat"><i class="fa-regular fa-comments"></i><em>Chat</em></router-link></li>
-        <!-- <li><router-link class="bt" to="/works"><i class="fa-regular fa-briefcase"></i><em>Works</em></router-link></li> -->
+        <!-- <li :class="isActive('chat')"><router-link class="bt" to="/works"><i class="fa-regular fa-briefcase"></i><em>Works</em></router-link></li> -->
         <li>
           <router-link v-if="!!$store.state.userInfo.uid" class="bt" to="/signout"><i class="fa-regular fa-right-from-bracket"></i><em>Logout</em></router-link>
           <router-link v-else class="bt" to="/signin"><i class="fa-regular fa-user"></i><em> Login</em></router-link>
